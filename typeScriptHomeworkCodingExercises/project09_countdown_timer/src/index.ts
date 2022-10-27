91408f119a67f06ac1c757fa4bd551a486110e42
asharib ahmed <asharibahmed143@gmail.com>
2022-10-27 17:18:29 +0500

    done with 10th project meta ts

--- typeScriptHomeworkCodingExercises/project09_countdown_timer/src/index.ts
+++ typeScriptHomeworkCodingExercises/project09_countdown_timer/src/index.ts
@@ -1,1 +1,18 @@
-console.log('Hello world!')
+
+var countDownDate = new Date().getTime();
+var x = setInterval(function () {
+    var now: number = new Date().getTime();
+
+    var distance: number = countDownDate - now;
+
+    var days: number = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours: number = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes: number = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds: number = Math.floor((distance % (1000 * 60)) / 1000);
+
+    console.log(`${days}days ${hours}hours ${minutes}minutes and ${seconds}seconds remaining...`)
+    if (distance < 0) {
+        clearInterval(x);
+        console.log("Your time is now");
+    }
+}, 1000);
